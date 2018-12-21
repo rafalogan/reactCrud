617b5d703d09a152a1e4a8a04b65cc9929a81618
RafaDraw <www.rafadraw@gmail.com>
2018-12-21 12:01:31 -0200

    init

---
+++ frontend/src/components/template/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+import './Logo.css';
+
+import logo from '../../assets/images/logo.png';
+
+export default props =>
+    <aside className="logo">
+        <Link to="/" className="logo">
+            <img src={logo} alt="logo" />
+        </Link>
+    </aside>
